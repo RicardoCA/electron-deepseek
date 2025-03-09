df142c4ad9401a1a45c0239b160143748a828c23
ricardoca <ricardocorreaandrade@proton.me>
2025-03-09 02:36:48 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false, // Mantém a segurança ao desativar o acesso ao Node.js
+    }
+  });
+
+  win.loadURL('https://chat.deepseek.com/');
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
